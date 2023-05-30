093d2bf801e74bcf53c9942a6697519749b7472e
Яременко Данило Вячеславович <danilyaremenko@gmail.com>
2023-05-30 19:50:22 +0300

    test: Add integration tests of balancer

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM golang:1.20 as build
 WORKDIR /go/src/practice-4
 COPY . .
 
+RUN go mod download
 RUN go test ./...
 ENV CGO_ENABLED=0
 RUN go install ./cmd/...
--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -1,6 +1,7 @@
 package integration
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
@@ -8,10 +9,19 @@ import (
 	"time"
 )
 
-const baseAddress = "http://balancer:8090"
+const (
+	baseAddress      = "http://balancer:8090"
+	numberOfRequests = 3
+)
+
+var serversPool = []string{
+	"server1:8080",
+	"server2:8080",
+	"server3:8080",
+}
 
 var client = http.Client{
-	Timeout: 3 * time.Second,
+	Timeout: 1 * time.Second,
 }
 
 func TestBalancer(t *testing.T) {
@@ -19,14 +29,55 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	flag.Parse()
+
+	var data = [3]string{"v1/capitals/berlin", "v1/planets/earth", "v1/data/qwerty"}
+
+	serverResponses := make(map[string]int)
+
+	for i := 0; i < numberOfRequests; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/%s", baseAddress, data[i]))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		server := resp.Header.Get("lb-from")
+		t.Logf("response from [%s]", server)
+		if server == "" {
+			t.Error("No lb-from header in response")
+		} else if server != serversPool[i] {
+			t.Errorf("Request was sent not to the expected server, sent to %s, expected %s", server, serversPool[i])
+		}
+
+		serverResponses[server]++
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	if len(serverResponses) != 3 {
+		t.Errorf("Expected responses from %v servers, but got responses from %v servers", len(serversPool), len(serverResponses))
+		return
+	}
+
+	t.Log("Balancer distributed requests among the following servers:")
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	if _, exists := os.LookupEnv("INTEGRATION_TEST"); !exists {
+		b.Skip("Integration test is not enabled")
+	}
+
+	startTime := time.Now()
+
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			b.Error("Error in benchmark: ", err)
+		}
+		resp.Body.Close()
+	}
+
+	elapsedTime := time.Since(startTime)
+
+	b.Log("Iterations: ", b.N)
+	b.Log("Time spent: ", elapsedTime)
 }
